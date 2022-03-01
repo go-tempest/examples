6de5b5ab53da6cef548cf7fa7c49b75256609b70
lauevan <liuyf.mizzle@foxmail.com>
2022-03-01 16:28:59 +0800

    删除用例

---
+++ user-name/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    _ "github.com/go-tempest/tempest/boostrap"
+    "os"
+    "os/signal"
+    "syscall"
+)
+
+func main() {
+
+    errChan := make(chan error)
+
+    go func() {
+        c := make(chan os.Signal, 1)
+        signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+        errChan <- fmt.Errorf("%s", <-c)
+    }()
+
+    err := <-errChan
+    fmt.Println(err)
+}
