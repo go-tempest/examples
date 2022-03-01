c437e928220d51609d79ab60dcb92753330bc72d
liuxiaxin <lxxwilliam@163.com>
2022-03-01 18:15:53 +0800

    user

---
+++ app/router/sys_router.go
@@ -0,0 +1,60 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-admin-team/go-admin-core/sdk/config"
+	jwt "github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth"
+	"github.com/go-admin-team/go-admin-core/sdk/pkg/ws"
+	"user/app/apis"
+	"user/common/middleware"
+	"user/common/middleware/handler"
+)
+
+func InitSysRouter(r *gin.Engine, authMiddleware *jwt.GinJWTMiddleware) *gin.RouterGroup {
+	g := r.Group("")
+	sysBaseRouter(g)
+
+	// 需要认证
+	sysCheckRoleRouterInit(g, authMiddleware)
+	return g
+}
+
+func sysBaseRouter(r *gin.RouterGroup) {
+
+	go ws.WebsocketManager.Start()
+	go ws.WebsocketManager.SendService()
+	go ws.WebsocketManager.SendAllService()
+
+	if config.ApplicationConfig.Mode != "prod" {
+		r.GET("/", handler.Ping)
+	}
+	r.GET("/info", handler.Ping)
+}
+
+func sysCheckRoleRouterInit(r *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
+	wss := r.Group("").Use(authMiddleware.MiddlewareFunc())
+	{
+		wss.GET("/ws/:id/:channel", ws.WebsocketManager.WsClient)
+		wss.GET("/wslogout/:id/:channel", ws.WebsocketManager.UnWsClient)
+	}
+
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/login", authMiddleware.LoginHandler)
+		// Refresh time can be longer than token timeout
+		v1.GET("/refresh_token", authMiddleware.RefreshHandler)
+	}
+	registerBaseRouter(v1, authMiddleware)
+}
+
+func registerBaseRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
+	api := apis.SysMenu{}
+	api2 := apis.SysDept{}
+	v1auth := v1.Group("").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
+	{
+		v1auth.GET("/roleMenuTreeselect/:roleId", api.GetMenuTreeSelect)
+		//v1.GET("/menuTreeselect", api.GetMenuTreeSelect)
+		v1auth.GET("/roleDeptTreeselect/:roleId", api2.GetDeptTreeRoleSelect)
+		v1auth.POST("/logout", handler.LogOut)
+	}
+}
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	"user/app/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-admin-team/go-admin-core/config/source/file"
+	"github.com/go-admin-team/go-admin-core/sdk"
+	"github.com/go-admin-team/go-admin-core/sdk/api"
+	"github.com/go-admin-team/go-admin-core/sdk/config"
+	"github.com/go-admin-team/go-admin-core/sdk/pkg"
+	"github.com/go-admin-team/go-admin-core/sdk/runtime"
+	"user/app/models"
+	"user/common/database"
+	"user/common/global"
+	common "user/common/middleware"
+	"user/common/middleware/handler"
+	"user/common/storage"
+	ext "user/config"
+)
+
+var configYml = "config/settings.yml"
+
+var AppRouters = make([]func(), 0)
+
+func init() {
+	AppRouters = append(AppRouters, router.InitRouter)
+}
+
+func main() {
+	setup()
+	run()
+}
+
+func setup() {
+	// 注入配置扩展项
+	config.ExtendConfig = &ext.ExtConfig
+	//1. 读取配置
+	config.Setup(
+		file.NewSource(file.WithPath(configYml)),
+		database.Setup,
+		storage.Setup,
+	)
+	//注册监听函数
+	queue := sdk.Runtime.GetMemoryQueue("")
+	queue.Register(global.LoginLog, models.SaveLoginLog)
+	queue.Register(global.OperateLog, models.SaveOperaLog)
+	queue.Register(global.ApiCheck, models.SaveSysApi)
+	go queue.Run()
+
+	usageStr := `starting api server...`
+	log.Println(usageStr)
+}
+
+func run() error {
+	if config.ApplicationConfig.Mode == pkg.ModeProd.String() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	initRouter()
+
+	for _, f := range AppRouters {
+		f()
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", config.ApplicationConfig.Host, config.ApplicationConfig.Port),
+		Handler: sdk.Runtime.GetEngine(),
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("listen: ", err)
+		}
+	}()
+	fmt.Println(pkg.Green("Server Run at:"))
+	fmt.Printf("-  Local:   http://localhost:%d/ \r\n", config.ApplicationConfig.Port)
+	fmt.Printf("-  Network: http://%s:%d/ \r\n", pkg.GetLocaHonst(), config.ApplicationConfig.Port)
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Printf("%s Shutdown Server ... \r\n", pkg.GetCurrentTimeStr())
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
+	return nil
+}
+
+var Router runtime.Router
+
+func initRouter() {
+	var r *gin.Engine
+	h := sdk.Runtime.GetEngine()
+	if h == nil {
+		h = gin.New()
+		sdk.Runtime.SetEngine(h)
+	}
+	switch h.(type) {
+	case *gin.Engine:
+		r = h.(*gin.Engine)
+	default:
+		log.Fatal("not support other engine")
+		os.Exit(-1)
+	}
+	if config.SslConfig.Enable {
+		r.Use(handler.TlsHandler())
+	}
+	//r.Use(middleware.Metrics())
+	r.Use(common.Sentinel()).
+		Use(common.RequestId(pkg.TrafficKey)).
+		Use(api.SetRequestLogger)
+
+	common.InitMiddleware(r)
+
+}
