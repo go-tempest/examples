acb8c8c879fba813c848ab0a09e7aace18fb5b1c
lauevan <liuyf.mizzle@foxmail.com>
2022-03-04 14:51:13 +0800

    修改

--- user/main.go
+++ user/main.go
@@ -4,6 +4,7 @@ import (
     "context"
     "fmt"
     "github.com/go-tempest/tempest/boostrap"
+    bc "github.com/go-tempest/tempest/boostrap/context"
     "google.golang.org/grpc"
     "net"
     "net/http"
@@ -17,45 +18,42 @@ import (
 )
 
 func main() {
+    boostrap.ResigerHook(boostrap.Post, func(bctx *bc.BootstrapContext) {
 
-    bctx := boostrap.GetContext()
-    defer func() {
-        _ = bctx.Logger.Sync()
-    }()
+        errc := make(chan error)
+        ctx := context.Background()
 
-    errc := make(chan error)
-    ctx := context.Background()
+        endpoints := new(endpoint.AssemblyEndpoint).Initialize()
 
-    endpoints := new(endpoint.AssemblyEndpoint).Initialize()
+        go func() {
+            handler := transport.CreateHttpHandler(ctx, endpoints)
+            errc <- http.ListenAndServe(":"+strconv.Itoa(bctx.AppConfig.Port), handler)
+        }()
 
-    go func() {
-        handler := transport.CreateHttpHandler(ctx, endpoints)
-        errc <- http.ListenAndServe(":"+strconv.Itoa(bctx.AppConfig.Port), handler)
-    }()
+        go func() {
+            grpcHandler := transport.CreateGrpcHandler(ctx, endpoints)
 
-    go func() {
-        grpcHandler := transport.CreateGrpcHandler(ctx, endpoints)
+            listener, err := net.Listen("tcp", ":"+strconv.Itoa(bctx.RegistrationConfig.Service.Port))
+            if err != nil {
+                errc <- err
+                return
+            }
 
-        listener, err := net.Listen("tcp", ":"+strconv.Itoa(bctx.RegistrationConfig.Service.Port))
-        if err != nil {
-            errc <- err
-            return
-        }
+            s := grpc.NewServer()
+            pb.RegisterUserServiceServer(s, grpcHandler)
+            err = s.Serve(listener)
+            if err != nil {
+                errc <- err
+                return
+            }
+        }()
 
-        s := grpc.NewServer()
-        pb.RegisterUserServiceServer(s, grpcHandler)
-        err = s.Serve(listener)
-        if err != nil {
-            errc <- err
-            return
-        }
-    }()
+        go func() {
+            c := make(chan os.Signal, 1)
+            signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+            errc <- fmt.Errorf("%s", <-c)
+        }()
 
-    go func() {
-        c := make(chan os.Signal, 1)
-        signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-        errc <- fmt.Errorf("%s", <-c)
-    }()
-
-    bctx.Logger.Error(<-errc)
+        bctx.Logger.Error(<-errc)
+    }).Start()
 }
